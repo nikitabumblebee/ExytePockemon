b1661325d71b971940f352de1087f2dab40baf6f
NikitaBumblebee <nikitashmelev7@gmail.com>
2022-04-09 09:36:37 +0700

    Add internet checker

--- ExytePockemon/AppDelegate.swift
+++ ExytePockemon/AppDelegate.swift
@@ -9,18 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     private(set) var pokemons: [Pokemon] = []
     
     private(set) var isLoaded = false
     
     private func loadData() {
         let networkManager = NetworkManager()
-        var pokemonCount = networkManager.getPokemonCount()
         DispatchQueue.global(qos: .utility).async {
-            while pokemonCount == 0 {
-                pokemonCount = networkManager.totalPokemonCount
-            }
+            let pokemonCount = networkManager.getPokemonCount()
             networkManager.getAllPokemons(totalPokemonCount: pokemonCount)
             while networkManager.isReady == false {
                 self.pokemons = networkManager.pokemons
@@ -28,27 +25,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.isLoaded = true
         }
     }
-
+    
+    private func checkInternet() -> Bool {
+        do {
+            try Network.reachability = Reachability(hostname: "www.google.com")
+            return true
+        } catch {
+            switch error as? Network.Error {
+            case let .failedToCreateWith(hostname)?:
+                print("Network error:\nFailed to create reachability object With host named:", hostname)
+            case let .failedToInitializeWith(address)?:
+                print("Network error:\nFailed to initialize reachability object With address:", address)
+            case .failedToSetCallout?:
+                print("Network error:\nFailed to set callout")
+            case .failedToSetDispatchQueue?:
+                print("Network error:\nFailed to set DispatchQueue")
+            case .none:
+                print(error)
+            }
+            return false
+        }
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        self.loadData()
+        let isActive = checkInternet()
+        if isActive {
+            self.loadData()
+        } else {
+            
+        }
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
--- Rest/NetworkManager.swift
+++ Rest/NetworkManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import PokemonAPI
-import SwiftUI
+import Combine
 
 public class NetworkManager {
     
@@ -17,21 +17,51 @@ public class NetworkManager {
     
     public private(set) var isReady = false
     
+    private var cancellables = Set<AnyCancellable>()
+    
     public func getPokemonCount() -> Int {
-        PokemonAPI().pokemonService.fetchPokemonList() { result in
-            switch result {
-            case .success(let a):
-                self.totalPokemonCount = a.count!
-                print(self.totalPokemonCount)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
+//        let pokemonService = PokemonAPI().pokemonService.fetchPokemonList()
+//        let cancellable = pokemonService
+//            .sink(receiveCompletion: { completion in
+//                    if case .failure(let error) = completion {
+//                        print(error.localizedDescription)
+//                    }
+//                }, receiveValue: { pokemonsResult in
+//                    if let totalPokemons = pokemonsResult.count {
+//                        self.totalPokemonCount = totalPokemons
+//                    } else {
+//                        self.totalPokemonCount = 0
+//                    }
+//                    print("Total pokemons: \(self.totalPokemonCount)")
+//                })
+//        let repoPublisher = pokemonService
+//            .map(\.count)
+//            .receive(on: DispatchQueue.main)
+        
+        var iteration = 0
+        while self.totalPokemonCount == 0 || iteration < 100 {
+            PokemonAPI().pokemonService.fetchPokemonList(completion: { result in
+                switch result {
+                case .success(let pokemonResult):
+                    if let totalPokemonCount = pokemonResult.count {
+                        self.totalPokemonCount = totalPokemonCount
+                    } else {
+                        self.totalPokemonCount = 0
+                    }
+                    print("Success \(self.totalPokemonCount)")
+                    iteration = 100
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            })
+            iteration += 1
         }
         return totalPokemonCount
     }
     
     public func getAllPokemons(totalPokemonCount: Int) {
-        DispatchQueue.global(qos: .userInitiated).async {
+        DispatchQueue.global(qos: .utility).async {
             var iteration = 0
             let downloadGroup = DispatchGroup()
             for i in 0...totalPokemonCount {
@@ -41,24 +71,7 @@ public class NetworkManager {
                     case .success(let pokemonResult):
                         iteration += 1
                         downloadGroup.leave()
-                        let pokemon = Pokemon()
-                        pokemon.name = pokemonResult.name
-                        pokemon.weight = pokemonResult.weight
-                        pokemon.height = pokemonResult.height
-                        pokemon.order = pokemonResult.order
-                        pokemon.baseExperience = pokemonResult.baseExperience
-                        for typeIterator in 0..<pokemonResult.types!.count {
-                            if let typeDescription = pokemonResult.types![typeIterator].type?.name {
-                                pokemon.types.append(typeDescription)
-                            }
-                        }
-                        for ability in 0..<pokemonResult.abilities!.count {
-                            if let abilityDescription = pokemonResult.abilities![ability].ability?.name {
-                                pokemon.abilities.append(abilityDescription)
-                            }
-                        }
-                        pokemon.frontSideImagePath = pokemonResult.sprites?.frontDefault
-                        pokemon.backSideImagePath = pokemonResult.sprites?.backDefault
+                        let pokemon = self.buildPokemon(pokemonResult: pokemonResult)
                         self.pokemons.append(pokemon)
                         print("Get items \(self.pokemons.count)")
                     case .failure(let error):
@@ -72,5 +85,27 @@ public class NetworkManager {
             }
             downloadGroup.wait()
         }
+    }
+    
+    private func buildPokemon(pokemonResult: PKMPokemon) -> Pokemon {
+        let pokemon = Pokemon()
+        pokemon.name = pokemonResult.name
+        pokemon.weight = pokemonResult.weight
+        pokemon.height = pokemonResult.height
+        pokemon.order = pokemonResult.order
+        pokemon.baseExperience = pokemonResult.baseExperience
+        for typeIterator in 0..<pokemonResult.types!.count {
+            if let typeDescription = pokemonResult.types![typeIterator].type?.name {
+                pokemon.types.append(typeDescription)
+            }
+        }
+        for ability in 0..<pokemonResult.abilities!.count {
+            if let abilityDescription = pokemonResult.abilities![ability].ability?.name {
+                pokemon.abilities.append(abilityDescription)
+            }
+        }
+        pokemon.frontSideImagePath = pokemonResult.sprites?.frontDefault
+        pokemon.backSideImagePath = pokemonResult.sprites?.backDefault
+        return pokemon
     }
 }
