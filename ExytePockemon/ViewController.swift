604169f201471376de651eedaaf3c999e870c437
NikitaBumblebee <nikitashmelev7@gmail.com>
2022-04-11 19:43:54 +0700

    Update collection view

--- ExytePockemon/ViewController.swift
+++ ExytePockemon/ViewController.swift
@@ -6,28 +6,54 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var favoriteLabel: UILabel!
-    @IBOutlet weak var contentScrollView: UIScrollView!
-    @IBOutlet weak var allCollectionView: UICollectionView!
-    @IBOutlet weak var favoriteCollectionView: UICollectionView!
+    @IBOutlet weak var pokemonCollection: UICollectionView!
     
     let viewModel = ParentControllerViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        pokemonCollection.register(UINib(nibName: String(describing: PockemonCellCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: PockemonCellCollectionViewCell.self))
+        pokemonCollection.register(UINib(nibName: String(describing: HeaderSupplementaryView.self), bundle: nil), forSupplementaryViewOfKind: "UICollectionElementKindSectionHeader", withReuseIdentifier: String(describing: HeaderSupplementaryView.reuseId))
         
-        favoriteCollectionView.delegate = self
-        favoriteCollectionView.dataSource = self
-        favoriteCollectionView.register(UINib(nibName: String(describing: PockemonCellCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: PockemonCellCollectionViewCell.self))
+        pokemonCollection.delegate = self
+        pokemonCollection.dataSource = self
+
+        checkSections()
+        self.pokemonCollection.reloadData()
+    }
+    
+    var collectionView: UICollectionView!
+    
+    var sections: [MSection]?
+    
+    func createCompositionalLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            let section = self.sections![sectionIndex]
+            
+            return self.createActiveChatSection()
+        }
         
-        allCollectionView.delegate = self
-        allCollectionView.dataSource = self
-        allCollectionView.register(UINib(nibName: String(describing: PockemonCellCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: PockemonCellCollectionViewCell.self))
+        return layout
+    }
+    
+    func createActiveChatSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(86))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets.init(top: 0, leading: 0, bottom: 8, trailing: 0)
+                
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .estimated(1))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
         
-        loadData()
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets.init(top: 66, leading: 20, bottom: 0, trailing: 20)
+        
+        return section
     }
 }
 
@@ -36,27 +62,40 @@ extension ViewController: UICollectionViewDelegate {
 }
 
 extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == self.favoriteCollectionView {
-            favoriteCollectionView.isHidden = self.viewModel.setFavoriteHidden()
-            favoriteLabel.isHidden = self.viewModel.setFavoriteHidden()
-            return self.viewModel.getFavoritePokemons().count
-        } else {
-            return self.viewModel.getUnlikedPokemons().count
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HeaderSupplementaryView.reuseId, for: indexPath) as? HeaderSupplementaryView else {
+            return HeaderSupplementaryView()
         }
+        headerView.titleLabel.text = sections![indexPath.section].title
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: 200, height: 40)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections![section].items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == allCollectionView {
-            return buildAllCollectionCell(indexPath: indexPath)
-        } else {
-            return buildFavoriteCell(indexPath: indexPath)
-        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PockemonCellCollectionViewCell.reuseId, for: indexPath) as! PockemonCellCollectionViewCell
+        let section = sections![indexPath.section]
+        let item = section.items[indexPath.item]
+        cell.navigateButton.tag = item.id
+        cell.navigateButton.addTarget(self, action: #selector(goDetail), for: .touchUpInside)
+        cell.favoriteStatusButton.addTarget(self, action: #selector(changeStatusAction), for: .touchUpInside)
+        cell.configureCell(pokemon: item)
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections!.count
     }
     
     @objc func changeStatusAction(sender: UIButton) {
-        self.favoriteCollectionView.reloadData()
-        self.allCollectionView.reloadData()
+        checkSections()
+        self.pokemonCollection.reloadData()
     }
     
     @objc func goDetail(sender: UIButton) {
@@ -70,16 +109,18 @@ extension ViewController: UICollectionViewDataSource {
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: favoriteCollectionView.frame.width / 2 - 10, height: favoriteCollectionView.frame.width / 2 - 10)
+        return CGSize(width: pokemonCollection.frame.width / 2 - 10, height: pokemonCollection.frame.width / 2 - 10)
     }
 }
 
 // MARK: Private functions
 extension ViewController {
-    private func loadData() {
-        DispatchQueue.main.async {
-            self.allCollectionView.reloadData()
-            self.favoriteCollectionView.reloadData()
+    private func checkSections() {
+        if viewModel.getFavoritePokemons().count == 0 {
+            sections = [MSection(title: "All", items: viewModel.getUnlikedPokemons())]
+        } else {
+            sections = [MSection(title: "Favorite", items: viewModel.getFavoritePokemons()),
+                        MSection(title: "All", items: viewModel.getUnlikedPokemons())]
         }
     }
     
@@ -88,20 +129,6 @@ extension ViewController {
         cell.navigateButton.tag = pokemon.id
         cell.navigateButton.addTarget(self, action: #selector(goDetail), for: .touchUpInside)
         cell.favoriteStatusButton.addTarget(self, action: #selector(changeStatusAction), for: .touchUpInside)
-        return cell
-    }
-    
-    private func buildAllCollectionCell(indexPath: IndexPath) -> PockemonCellCollectionViewCell {
-        var cell = allCollectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PockemonCellCollectionViewCell.self), for: indexPath) as! PockemonCellCollectionViewCell
-        let pokemon = self.viewModel.getUnlikedPokemons()[indexPath.row]
-        cell = createPokemonCell(cell: cell, pokemon: pokemon)
-        return cell
-    }
-    
-    private func buildFavoriteCell(indexPath: IndexPath) -> PockemonCellCollectionViewCell {
-        var cell = favoriteCollectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PockemonCellCollectionViewCell.self), for: indexPath) as! PockemonCellCollectionViewCell
-        let pokemon = self.viewModel.getFavoritePokemons()[indexPath.row]
-        cell = createPokemonCell(cell: cell, pokemon: pokemon)
         return cell
     }
 }
